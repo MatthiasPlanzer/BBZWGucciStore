5c263345f9c0fb3827c42cdb64394d07f20bea03
Matthias Planzer <mathias.planzer@kauz.ch>
2021-06-22 11:39:29 +0200

    massive cleanups, removed pinning of repositories
    nodejs cleanups, general cleanups,

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,19 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProductService } from '../products.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent {
   product$ = this.productService.getProductByID(+this.route.snapshot.params.id);
-  constructor(private route: ActivatedRoute, public productService: ProductService) {
-  }
-  ngOnInit() {
-    console.log(this.route.snapshot.params.id);
-    this.route.paramMap.subscribe(() => {
-    });
-  }
+  constructor(private route: ActivatedRoute, public productService: ProductService) {}
 }
